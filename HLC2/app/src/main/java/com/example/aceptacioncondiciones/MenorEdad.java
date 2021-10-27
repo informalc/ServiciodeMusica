a340bd2b6dcb72ce03429f71d40cd3f864ba6749
Fabri <43851592+Titillo@users.noreply.github.com>
2021-10-27 14:26:40 +0200

    Add files via upload
    Tarea 3 y 4

---
+++ HLC2/app/src/main/java/com/example/aceptacioncondiciones/MenorEdad.java
@@ -0,0 +1,14 @@
+package com.example.aceptacioncondiciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenorEdad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menor_edad);
+    }
+}

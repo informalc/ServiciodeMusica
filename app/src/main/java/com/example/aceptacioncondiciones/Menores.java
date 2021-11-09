5ce8484d90877e955fbf1b83c67c75d92eb50951
Titillo <43851592+Titillo@users.noreply.github.com>
2021-11-09 16:24:31 +0100

    Tarea 3

---
+++ app/src/main/java/com/example/aceptacioncondiciones/Menores.java
@@ -0,0 +1,14 @@
+package com.example.aceptacioncondiciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menores);
+    }
+}

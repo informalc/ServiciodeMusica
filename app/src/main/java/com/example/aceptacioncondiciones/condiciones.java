05f86837fe26d3dac7a3a52a4ef45e0dbcb031d8
Titillo <43851592+Titillo@users.noreply.github.com>
2021-11-09 16:28:09 +0100

    Tarea 3 y 4

---
+++ app/src/main/java/com/example/aceptacioncondiciones/condiciones.java
@@ -0,0 +1,14 @@
+package com.example.aceptacioncondiciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class condiciones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_condiciones);
+    }
+}

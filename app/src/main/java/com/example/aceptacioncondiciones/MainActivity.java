d811170d0afebc83afb137ada6c04e608df93ec4
gsusalcaraz <gsus.alcaraz@gmail.com>
2021-11-12 18:48:10 +0100

    Validación campos de edad y nombre
    Se validan que los campos no estén vacíos, que sea una edad correcta, y se controla si es +18.

--- app/src/main/java/com/example/aceptacioncondiciones/MainActivity.java
+++ app/src/main/java/com/example/aceptacioncondiciones/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.aceptacioncondiciones;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView nombre, edad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nombre = findViewById(R.id.txtNombre);
+        edad = findViewById(R.id.txtEdad);
+    }
+
+    public void btnVerificar (View view){
+
+        //Verificar que los campos no están vacíos
+        if (nombre.getText().length() != 0 && edad.getText().length() != 0) {
+            if (Integer.parseInt(edad.getText().toString()) > 100 || Integer.parseInt(edad.getText().toString()) < 0){
+                Toast.makeText(MainActivity.this, "Introduce edad válida", Toast.LENGTH_LONG).show();
+            } else if(Integer.parseInt(edad.getText().toString()) > 17){
+                // Llamar Activity 2
+                Toast.makeText(MainActivity.this, "+18", Toast.LENGTH_LONG).show();
+            } else{
+                // Llamar Activiy Menores
+                Toast.makeText(MainActivity.this, "-18", Toast.LENGTH_LONG).show();
+            }
+        }else if(nombre.getText().length() == 0 && edad.getText().length() == 0){ // Nombre y Edad están vacíos
+            Toast.makeText(MainActivity.this, "Campos vacíos", Toast.LENGTH_LONG).show();
+        }else if(nombre.getText().length() == 0 && edad.getText().length() != 0){ //Nombre vacío
+            Toast.makeText(MainActivity.this, "Introduce nombre", Toast.LENGTH_LONG).show();
+        } else{ //Edad vacía
+            Toast.makeText(MainActivity.this, "Introduce edad", Toast.LENGTH_LONG).show();
+        }
+
     }
 }
